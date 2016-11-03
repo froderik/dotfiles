43b05945253420199b6989564f143742225ba46d
Fredrik Rubensson <fredrik.rubensson@eniro.com>
2016-11-03 09:04:20 +0100

    screenlayouts for eniro

---
+++ .screenlayout/eniro.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output eDP1 --off --output DP1 --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI2 --off --output HDMI1 --off --output DP2 --off
+killall conky
+conky &
